f9fdfc7604be7e550796984f2ed154bc17748eec
HaneenQandeel <haneenqandeel2@gmail.com>
2019-01-07 12:13:11 +0200

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { NavComponent } from './nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
+import { PromoAreaComponent } from './promo-area/promo-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    NavComponent,
+    BannerComponent,
+    PromoAreaComponent
   ],
   imports: [
     BrowserModule,
